6970bc607bac5ed3e7d3f83f484da13a127d1120
Umer-Shahzad112 <109405779+Umer-Shahzad112@users.noreply.github.com>
2022-08-21 10:01:30 -0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -116,12 +116,8 @@ if available:
                         col1 ,col2 = st.columns(2)
                         with col1:
                             st.slider('X lower limit',float(xmin),float(xmax),key='xmin')
-                            if st.checkbox('Set X log scale'):
-                                plt.xscale('log')
                         with col2:
-                            st.slider('X upper limit',float(xmin),float(xmax),key= 'xmax')
-                            if st.checkbox('Set Y log scale'):
-                                plt.yscale('log')
+                            st.slider('X upper limit',float(xmin),float(xmax),key= 'xmax'))
                         plt.xlim(st.session_state.xmin,st.session_state.xmax)
                     # except:
                     #     st.warning(body = 'You can not set xlim',icon='⚠️')
